2fd26acdf8beb9a0bd0ee8a88e14413ab9675053
joserbngithub <joserbn@hotmail.com>
2021-07-13 07:18:56 +0100

    mudei texto para cada sorteio.

--- main.ts
+++ main.ts
@@ -5,23 +5,23 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         music.baDing.play()
     }
     if (dado == 2) {
-        game.splash("1 >>>>> NUMERO 2")
+        game.splash("2 >>>>> NUMERO 2")
         music.baDing.play()
     }
     if (dado == 3) {
-        game.splash("1 >>>>> NUMERO 3")
+        game.splash("3 >>>>> NUMERO 3")
         music.baDing.play()
     }
     if (dado == 4) {
-        game.splash("1 >>>>> NUMERO 4")
+        game.splash("4 >>>>> NUMERO 4")
         music.baDing.play()
     }
     if (dado == 5) {
-        game.splash("1 >>>>> NUMERO 5")
+        game.splash("5 >>>>> NUMERO 5")
         music.baDing.play()
     }
     if (dado == 6) {
-        game.splash("1 >>>>> NUMERO 6")
+        game.splash("6 >>>>> NUMERO 6")
         music.baDing.play()
     }
 })
